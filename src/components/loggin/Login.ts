47684fe402af661f463b22a2720a34e24b4170b4
Luna Sarmiento <selene942008@hotmail.com>
2021-10-12 16:38:15 +0200

    login working

--- src/components/loggin/Login.ts
+++ src/components/loggin/Login.ts
@@ -1,5 +1,18 @@
 import { mixLogInInt } from "../../usefull/interfaces";
 
-export const handleSubmit = ({ e, logIn }: mixLogInInt) => {
+export const handleSubmit = async ({ e, logIn }: mixLogInInt) => {
   e.preventDefault();
+  try {
+    let response = await fetch("http://localhost:3001/users/session", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(logIn),
+    });
+    if (response.ok) {
+      let data = await response.json();
+      localStorage.setItem("token", data.refreshToken);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
