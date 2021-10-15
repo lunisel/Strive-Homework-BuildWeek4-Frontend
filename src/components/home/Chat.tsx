664b70e091920075f5fc4bb844e4731b22e6a77d
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-10-15 15:40:01 +0100

    ok

--- src/components/home/Chat.tsx
+++ src/components/home/Chat.tsx
@@ -1,21 +1,22 @@
-import { GoSearch } from "react-icons/go";
+import { GoSearch } from 'react-icons/go'
 import {
   BsThreeDotsVertical,
   BsEmojiLaughing,
   BsFillMicFill,
-} from "react-icons/bs";
-import { FiPaperclip } from "react-icons/fi";
-import { Form } from "react-bootstrap";
-import SingleChat from "./SingleChat";
-import { useEffect, useState, KeyboardEvent } from "react";
-import { changeNewMessage } from "../../redux/actions/chats";
-import { useDispatch, useSelector } from "react-redux";
+} from 'react-icons/bs'
+import { FiPaperclip } from 'react-icons/fi'
+import { Form } from 'react-bootstrap'
+import SingleChat from './SingleChat'
+import { useEffect, useState, KeyboardEvent } from 'react'
+import { changeNewMessage } from '../../redux/actions/chats'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   membersInt,
   reduxStateInt,
   roomsInt,
   UserInt,
-} from "../../usefull/interfaces";
+} from '../../usefull/interfaces'
+import { setTimeout } from 'timers'
 
 // const sendMessageToIng = async () => {
 //   const body = {
@@ -44,134 +45,145 @@ import {
 // };
 
 const Chat = () => {
-  const [dropdown, setDropdown] = useState<boolean>(false);
-  const [otherUser, setOtherUser] = useState<membersInt | undefined>(undefined);
-  const [message, setMessage] = useState<string>("");
+  const [dropdown, setDropdown] = useState<boolean>(false)
+  const [otherUser, setOtherUser] = useState<membersInt | undefined>(undefined)
+  const [message, setMessage] = useState<string>('')
 
   const dispatch = useDispatch()
 
   const selectedChat: roomsInt | null = useSelector(
     (state: reduxStateInt) => state.chats.selectedChat
-  );
+  )
 
-  console.log("selected",selectedChat);
+  console.log('selected', selectedChat)
 
   const user: UserInt | null = useSelector(
     (state: reduxStateInt) => state.user.currentUser
-  );
+  )
 
   const selectedChatUser = () => {
     let otherUserChat: membersInt | undefined = selectedChat?.members?.find(
       (u) => u._id !== user?._id
-    );
-    setOtherUser(otherUserChat);
-  };
+    )
+    setOtherUser(otherUserChat)
+  }
 
   const sendMessageToChat = async (message: string) => {
     try {
       const membersArray = await selectedChat!.members.filter(
         (member) => member._id !== user!._id
-      );
+      )
       const arrayOfMembersIds = await membersArray.map((member) =>
         member._id.toString()
-      );
+      )
       const sendBody = {
         members: arrayOfMembersIds,
         message: {
           content: {
             text: message,
           },
-        }, 
-      };
+        },
+      }
       let response = await fetch(`${process.env.REACT_APP_BE_URL}/chats`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json", 
-          "Authorization": `Bearer ${localStorage.getItem("token")}`,
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
         body: JSON.stringify(sendBody),
-      });
+      })
       if (response.ok) {
-        let updatedUser = await response.json();
-        dispatch(changeNewMessage({
-          status: true, 
-          content: message,
-          _id: selectedChat!._id, 
-        }))
-        console.log(updatedUser);
+        let updatedUser = await response.json()
+        dispatch(
+          changeNewMessage({
+            status: true,
+            content: message,
+            _id: selectedChat!._id,
+          })
+        )
+        setTimeout(() => {
+          dispatch(
+            changeNewMessage({
+              status: false,
+              content: '',
+              _id: '',
+            })
+          )
+        }, 1000)
+        console.log(updatedUser)
       } else {
-        console.log("SOMETHING WENT WRONG!");
+        console.log('SOMETHING WENT WRONG!')
       }
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
 
   useEffect(() => {
-    selectedChatUser();
+    selectedChatUser()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedChat]);
+  }, [selectedChat])
 
   return (
-    <div className='chat-big-cont'>
+    <div className="chat-big-cont">
       {selectedChat ? (
         <>
-          <div className='chat-receiver-bar'>
-            <div className='img-name-container ml-4'>
+          <div className="chat-receiver-bar">
+            <div className="img-name-container ml-4">
               <img
                 src={otherUser?.avatar}
-                alt='profile'
-                className='h-75 w-auto rounded-circle mr-3'
+                alt="profile"
+                className="h-75 w-auto rounded-circle mr-3"
               />
-              <span className='name-receiver'>{otherUser?.name}</span>
+              <span className="name-receiver">{otherUser?.name}</span>
             </div>
-            <div className='search-dots-container mr-3 position-relative'>
-              <GoSearch className='search-dots mr-2' />
-              <div className={dropdown ? "right-side active" : "right-side"}>
+            <div className="search-dots-container mr-3 position-relative">
+              <GoSearch className="search-dots mr-2" />
+              <div className={dropdown ? 'right-side active' : 'right-side'}>
                 <BsThreeDotsVertical
-                  className='search-dots'
+                  className="search-dots"
                   onClick={() => {
-                    setDropdown(!dropdown);
+                    setDropdown(!dropdown)
                   }}
                 />
                 {dropdown ? (
-                  <div className='dropdown-container chat'>
-                    <div className='dropdown-links'>Info contact</div>
-                    <div className='dropdown-links'>Delete messages</div>
-                    <div className='dropdown-links'>Delete chat</div>
+                  <div className="dropdown-container chat">
+                    <div className="dropdown-links">Info contact</div>
+                    <div className="dropdown-links">Delete messages</div>
+                    <div className="dropdown-links">Delete chat</div>
                   </div>
                 ) : (
-                  <div className='chat-cont-without-selected-chat'></div>
+                  <div className="chat-cont-without-selected-chat"></div>
                 )}
               </div>
             </div>
           </div>
           <SingleChat />
-          <div className='bottom-input-text'>
-            <BsEmojiLaughing className='bottom-icons-bar mr-3' />
-            <FiPaperclip className='bottom-icons-bar mr-2' />
+          <div className="bottom-input-text">
+            <BsEmojiLaughing className="bottom-icons-bar mr-3" />
+            <FiPaperclip className="bottom-icons-bar mr-2" />
             <Form.Control
-              type='text'
-              placeholder='Write a message'
-              className='search-input chat'
+              type="text"
+              placeholder="Write a message"
+              className="search-input chat"
               value={message}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                 setMessage(e.currentTarget.value)
               }
               onKeyPress={(e: KeyboardEvent) => {
-                if (e.key === "Enter") {
-                  sendMessageToChat(message);
+                if (e.key === 'Enter') {
+                  sendMessageToChat(message)
                 }
               }}
             />
-            <BsFillMicFill className='mic-input' />
+            <BsFillMicFill className="mic-input" />
           </div>
         </>
       ) : (
-        <div className='chat-cont-without-selected-chat'></div>
+        <div className="chat-cont-without-selected-chat"></div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Chat;
+export default Chat
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,73 +1,84 @@
-import { useEffect} from "react";
-import { RouteComponentProps } from "react-router-dom";
-import { Row, Col } from "react-bootstrap";
-import Sidebar from "./Sidebar";
-import Chat from "./Chat";
-import "./styles.css";
-import { io } from "socket.io-client";
+import { useEffect } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+import { Row, Col } from 'react-bootstrap'
+import Sidebar from './Sidebar'
+import Chat from './Chat'
+import './styles.css'
+import { io } from 'socket.io-client'
+import { useSelector } from 'react-redux'
+import { newMessageInt, reduxStateInt } from '../../usefull/interfaces'
 
 const Home = (props: RouteComponentProps) => {
-  const ADDRESS = process.env.REACT_APP_BE_URL!;
-  const socket = io(ADDRESS, { transports: ["websocket"] }); 
+  const ADDRESS = process.env.REACT_APP_BE_URL!
+  const socket = io(ADDRESS, { transports: ['websocket'] })
+
+  const newMessage: newMessageInt | null = useSelector(
+    (state: reduxStateInt) => state.chats.newMessage
+  )
 
   useEffect(() => {
-    console.log("HELLO THIS IS THE USEEFFECT AT HOME");
-    loadHome();
-    loadSocket();
+    console.log('HELLO THIS IS THE USEEFFECT AT HOME')
+    loadHome()
+    loadSocket()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
   const loadSocket = () => {
-    socket.on("connect", () => {
-      console.log("Connection established!");
-      console.log("🚀SOCKET.ID=>", socket.id);
-    });
-    socket.on("joinedRoom", () => {
-      console.log("Now I've joined the rooms!");
-    });
+    socket.on('connect', () => {
+      console.log('Connection established!')
+      console.log('🚀SOCKET.ID=>', socket.id)
+    })
+    socket.on('joinedRoom', () => {
+      console.log("Now I've joined the rooms!")
+    })
     socket.on('message', (newMessageJustReceived) => {
       console.log("message received! let's post it in the window...")
       console.log(newMessageJustReceived)
     })
-    //socket.emit("sendmessage", { message: message, room: selectedChat!._id })
-  };
+
+    if (newMessage?.status === true) {
+      socket.emit('sendmessage', {
+        message: newMessage.content,
+        room: newMessage._id,
+      })
+    }
+  }
 
   const loadHome = async () => {
-    await getMyProfile();
-    console.log("HOME LOADED!");
-  };
-
+    await getMyProfile()
+    console.log('HOME LOADED!')
+  }
 
   const getMyProfile = async () => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token')
     const response = await fetch(`http://localhost:3001/users/me`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    });
+    })
     if (response.ok) {
-      const profile = await response.json();
-      console.log("HERE I AM=>", profile);
+      const profile = await response.json()
+      console.log('HERE I AM=>', profile)
     } else {
-      props.history.push("/login")
+      props.history.push('/login')
     }
-  };
+  }
 
   return (
-    <div className='home-cont'>
-      <div className='green-background-line'></div>
-      <div className='content-home-cont'>
-        <Row className='m-0 h-100'>
-          <Col xs={4} className='side-bar p-0'>
+    <div className="home-cont">
+      <div className="green-background-line"></div>
+      <div className="content-home-cont">
+        <Row className="m-0 h-100">
+          <Col xs={4} className="side-bar p-0">
             <Sidebar />
           </Col>
-          <Col xs={8} className='chat-container p-0'>
+          <Col xs={8} className="chat-container p-0">
             <Chat />
           </Col>
         </Row>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
